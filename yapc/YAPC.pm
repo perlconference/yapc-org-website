36a519c192d4b66bd845343e93162d62967a0e21
cbrandtbuffalo <cbrandtbuffalo@e9855c8f-261f-0410-b733-3d5002d6ba3b>
2006-10-06 16:32:50 +0000

    First check-in of modified version. A bunch of new information added. Still far from complete.

--- yapc/YAPC.pm
+++ yapc/YAPC.pm
@@ -1,8 +1,10 @@
 package YAPC;
 
 use strict;
-$YAPC::VERSION = '1.01';
+$YAPC::VERSION = '1.02';
+
 1;
+
 __END__
 
 =head1 NAME
@@ -29,12 +31,224 @@ language.  These conferences have been carried out in a stronghold of
 interest by small local core groups, with the aid of the local users
 groups and members of the community.
 
-=head1 SPONSORSHIP
+=head1 SELECTING SITES
+
+=head2 General Criteria
+
+After you have a list of potential sites and you've gotten some detail
+on them, ask yourself some questions to narrow it down:
+
+=over
+
+=item *
+
+Would I choose this if I were on the selection committee?
+
+=item *
+
+Would I personally want to go there?
+
+=item *
+
+Which would be easiest for me to work as coordinator?
+
+=back
+
+=head1 Preparing and Organizing the Conference
+
+=head2 High Level Overview
+
+At its core, hosting a YAPC is providing a location for members of the Perl community to get together. Although YAPCs have grown to include many other features, this is the main goal. Given this, there are a few things you must have ready, at a minimum, to make YAPC happen:
+
+=over
+
+=item * A venue
+
+This is the place where everyone will get together for the sessions.
+
+=item * Accommodations for attendees
+
+This is where everyone will stay. A primary conference hotel and a less expensive dorm-style option are usually provided. Although in a pinch, people can fend for themselves, there is often quite a bit of unofficial gathering at the hotel and this can't happen if people are staying at different places.
+
+=item * Speakers
+
+These are the people who will give the presentations that the attendees want to see. It is best if you can get some people well-known in the Perl community because this will drive attendance.
+
+=item * Attendees
+
+These are the people who will attend the conference. To get them to come, you need all of the above in place and you need to communicate it so the attendees know all the details.
+
+=back
+
+That's it. The rest is really gravy--quite a bit of important gravy--but just gravy. When you are planning a YAPC, keep this list in mind. These are your "big rocks" and if you are spending a ton of time on something and one of these are not done, you need to reconsider your priorities. On the flip side, if you have all of these in place, don't stress too much if something else falls through.
+
+=head2 Conference Timeline
+
+Below are the main events in the conference life-cycle. Refer to this as you prepare for the conference to make sure you are on track and haven't missed anything. Refer to the remaining documentation for details on each step.
+
+XXX Note: you need to time the end date for presentations in line with registration to allow people to see the program. Also deal with the question of registration/payment for speakers.
+
+=over
+
+=item -14 Start preparing your bid
+
+=item -12 Submit bid
+
+=item -10 Win bid
+
+=item -9 Set date for conference
+
+=item -9 Book keynote speakers
+
+=item -6 Call for presentations
+
+=item -4 End date for presentations
+
+=item -3 Publish schedule of talks
+
+=item -3 Open registration
+
+=back
+
+=head2 Conference Planning Team and Responsibilities
+
+=head3 Responsibilities of the Primary Organizer
+
+Most of this document uses the familiar 'you' when referring to tasks. However, if you are the primary organizer, this really means them. Pulling off a YAPC really requires that you have a team of dedicated people helping you. If you don't have a team of helpers, you really need to get one.
+
+Ask your local Perl Mongers group to get involved. Ask on the YAPC list for things that can be done remotely. As the event approaches, your goal is to have no tasks assigned to you (or very few). That way, you can jump in and help with whatever needs attention without abandoning something. You need to be the project manager, directing the tasks and reviewing the work, but not doing much yourself. Believe me, just coordinating things will take up plenty of your time.
+
+YAPC in Buffalo had 4 coordinators in the main group working throughout the planning process and about 20+ volunteers during the conference. In Toronto, nearly all of the Toronto.pm members helped with planning and execution, and this was between 15 and 20 people. Remember, many hands make light work (as long as you are coordinating and the hands know what to do).
+
+Above all, don't be a martyr. This event should be fun for you, your team, and everyone else involved.
+
+=head2 Picking the Venue
+
+=head3 Venue Cost
+
+YAPC is a grass-roots conference and we do everything we can to keep costs low. This allows people like open source developers to attend on a limited budget. Depending on how things go, the venue is likely to be your highest cost item and it can exert significant pressure on your attempt to keep the cost down.
+
+As an organization, TPF basically has two choices with regard to venues:
+
+=over
+
+=item 1) Work all of our connections and get venues donated or offered for very low cost because of our status as a not-for-profit.
+
+=item 2) Book with standard conference venues and charge high attendance fees to support the venue.
+
+=back
+
+Thus far, we have been able to get by with option 1 and we'd like to keep it that way. The conferences committee will certainly take this into account when selecting the host group each year.
+
+=head3 Booking the Venue
+
+Before you submit your bid, you are going to need to do a considerable amount of research into venues. This venue information will then be included in your bid. However, since you don't know if you are going to win the bid, you can't actually book the venue.
+
+When you are investigating venues make sure you find out the following:
+
+=over
+
+=item * What is required to book the venue? Deposit, signatures, insurance, etc.
+
+=item * How long before the event you can book?
+
+=item * Is someone else looking at your dates?
+
+=back
+
+Once you win the bid, one of your first tasks is to book the venue and that should be easy if you have the answers to the above.
+
+Why is this so important? Several YAPCs have had venues booked based on verbal commitments only to have things fall through because of mis-understandings. Get as much information as possible to avoid this.
+
+=head3 Backup
+
+When you are preparing your bid, you will likely investigate many different venues in your area. After you and your group rank your list and you pick your top choice, get all of the same information on number 2 as well. This will give you a backup plan if the initial venue falls through. And it's much easier to go ahead with your second choice if you already have all of the information.
+
+Several YAPCs have had the primary venue fall through, so please don't skip this step.
+
+=head2 Attendee Accommodations
+
+=head3 Hotel
+
+You'll need to designate a hotel as the primary conference hotel. People like to congregate and even if the hotel isn't right next to the venue, if you assign one that's where everyone will go.
+
+If the hotel isn't immediately adjacent to the venue, you need to work out all the details to get people from the hotel to the venue and back.
+
+When you pick the hotel, the most important points are internet access and price.
+
+discount/
+
+making promise for number of rooms.
+
+
+=head3 Dorms or Dorm-style Housing
+
+
+
+=head2 Food and Beverages
+
+=head3 Lunches
+
+If you don't know the area around the venue well, start with Google Maps and create a map with key points such as restaurants, drug stores, grocery stores, etc. marked. Then make a road-trip with your team and scout things out. See if things are really there and are within walking distance.
+
+=head3 Conference Dinner and Auction
+
+I recommend going with the on-campus event. Make it easy for people.
+
+Regardless of the venue, some people won't come to the dinner and some will leave after the dinner. Even on the boat, many people didn't participate in the auction even though they were a captive audience.
+
+People want an opportunity to hang out, talk perl, and help out by bidding on cool books. Your campus option provides all of those.
+
+As far as numbers, I think most do attend the dinner, but not all. I don't have firm numbers for you and caterers often need a firm number. We asked during check-in if the person planned on attending the dinner. Also, you might want to collect dietary preferences. You'll have some veggies, some vegans, and one or two kosher, and possibly some food allergies. Keep this in mind for any other food items too.
+
+=head1 Budgets and Money
+
+=head2 Registration Fees
+
+One source of income is the registration fee. Ideally you want this fee to cover the per-person costs so the attendee is, at least, paying their way for your attendee-imposed expenses. So one way to come up with a fee is to add up your costs on a per-attendee basis.
+
+YAPC has kept this at the very low $85 for many years, and the goal of many of the tips offered in this document is to keep that price point as low as possible. We don't every want the registration fee to be a barrier to someone coming. The low fee also opens the door to students and other non-corporate types who want to get a taste of what Perl is all about.
+
+=head3 Exemptions
+
+Typically presenters and keynote speakers are given free registration, since that's all we really have to offer. However, talks can be a continuum that range from a 5-minute lightning talk to a full day seminar. So you need to decide what your cut-off is. One system might be:
+
+  Keynotes: free
+  Talks 20 minutes or more: free
+  Talks less than 20 minutes, including lightning talks: full registration
+
+Around the 20 minute talk area, if the speaker is only doing one 20-minute talk, you might consider charging. This is an area with some flexibility.
+
+You may also want to grant free registration to some of your volunteers. Note that you are incurring cost for all of these people for things like food, a t-shirt, etc. so don't grant free admission to everyone. Save it as a reward for volunteers who helped consistently, meaningfully, and throughout the planning process.
+
+Any profits from YAPC benefit The Perl Foundation, so you can mention to all of the groups above that if they choose to pay, you'll let them. In some cases they'll be happy to pay their way and allow some extra money to trickle to TPF.
+
+=head2 Sponsorship
 
 Corporate sponsorship is used to lower the cost of participation and
 raise the quality of the services offered, in return for good will,
 recognition, and the ability to help create the atmosphere where
 conversations can flourish, and keep the endeavor vital.
+
+=head1 RUNNING THE CONFERENCE
+
+=head2 Opening Address
+
+You have a few responsibilities in your opening address:
+
+=over
+
+=item *
+
+Thank your sponsors and everyone who helped. People gave money and time to
+make your event happen. All they get is some recognition and the worst
+thing you can do is forget them.
+
+=item *
+
+When you are done, thank your sponsors again. You can't do this too much.
+
+=over
 
 =head1 HISTORY
 
@@ -67,9 +281,17 @@ Jouke Visser, and a large and dedicated crew from the Netherlands,
 created an excellent atmosphere for both talks and discussion, raising
 the overall standard once again.
 
-=head1 AUTHOR
+=head1 TODO
 
-Kevin A. Lenzo <lenzo@yetanother.org>. 
+* Money issues: insurance coverage; liability; TPF will cover costs
+in the event of something bad happening; tax exemption;
+
+=cut
+
+=head1 Authors
+
+Jim Brandt
+Kevin A. Lenzo
 
 Copyright (C) 2000, 2001 Yet Another Society.  Free use and
 redistribution of this document is encouraged under the same terms as
